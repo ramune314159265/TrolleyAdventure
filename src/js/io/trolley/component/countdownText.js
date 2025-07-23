86d0a02bfa6142d8319c932a9cd8b27aa4e4b564
ramune314159265 <ramune314159265@gmail.com>
2025-07-23 14:08:35 +0900

    countdown

---
+++ src/js/io/trolley/component/countdownText.js
@@ -0,0 +1,32 @@
+import { TrolleyIO } from '..'
+import { MainText } from './mainText'
+
+export class CountdownText extends MainText {
+	constructor({ styleOverride, additionalScale }) {
+		super({ styleOverride })
+		this.additionalScale = additionalScale
+		this.visible = false
+		this.ticker = () => { }
+	}
+	start({ periodMs }) {
+		const endsMs = performance.now() + periodMs
+		this.visible = true
+		this.ticker = () => {
+			const remain = endsMs - performance.now()
+			if (remain < 0) {
+				this.visible = false
+				TrolleyIO.instance.app.ticker.remove(this.ticker)
+				return
+			}
+			const second = Math.ceil(remain / 1000)
+			const remainder = remain / 1000 - Math.floor(remain / 1000)
+			this.text = second
+			this.scale.set(1 + this.additionalScale * remainder)
+		}
+		TrolleyIO.instance.app.ticker.add(this.ticker)
+	}
+	abort() {
+		this.visible = false
+		TrolleyIO.instance.app.ticker.remove(this.ticker)
+	}
+}
