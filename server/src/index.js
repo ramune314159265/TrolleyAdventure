64a739ad9f978c29b928c2065552f8bd04ce899d
ramune314159265 <ramune314159265@gmail.com>
2025-08-23 23:45:45 +0900

    logger

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import path from 'path'
 import { apiRoute } from './api.js'
 
 const app = new Hono()
+app.use(logger())
 
 app.get('/*', serveStatic({ root: path.join(import.meta.dirname, '../../dist') }))
 
