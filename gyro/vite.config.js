96c560c09f93b4ad4774464c4a696a7af6c21bb6
ramune314159265 <ramune314159265@gmail.com>
2025-09-04 02:42:51 +0900

    pwa

--- gyro/vite.config.js
+++ gyro/vite.config.js
@@ -1,6 +1,29 @@
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: './'
+	base: './',
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			manifest: {
+				name: 'シンクロナイズドスペーシャルハイウェイ ジャイロ',
+				short_name: 'SSHジャイロ',
+				description: 'SSH スマホで傾き検知',
+				theme_color: '#000',
+				display: 'fullscreen',
+				lang: 'ja-jp',
+				orientation: 'landscape-primary',
+				icons: [
+					{
+						src: 'icon_144.png',
+						sizes: '144x144',
+						type: 'image/png'
+					}
+				]
+			},
+		})
+	],
 })
