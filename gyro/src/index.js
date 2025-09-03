0bfae4438270cb4a9462f2b7a909b76d2f6eeb8b
ramune314159265 <ramune314159265@gmail.com>
2025-09-03 17:16:23 +0900

    gyro 送信

---
+++ gyro/src/index.js
@@ -0,0 +1,72 @@
+let gamma = 0
+let ws = null
+let pastInputStatus = {
+	left: false,
+	right: false,
+	confirm: false
+}
+
+const inputChange = (key, newState) => {
+	if (!ws) {
+		return
+	}
+	switch (true) {
+		case (key === 'right' || key === 'left') && !newState:
+			ws.send(JSON.stringify({
+				event: 'center'
+			}))
+			break
+
+		case key === 'right':
+			ws.send(JSON.stringify({
+				event: 'right'
+			}))
+			break
+
+		case key === 'left':
+			ws.send(JSON.stringify({
+				event: 'left'
+			}))
+			break
+
+		default:
+			break
+	}
+}
+
+const loop = () => {
+	const inputStatus = {
+		right: 9 < gamma,
+		left: gamma < -9,
+	}
+	Object.entries(inputStatus).forEach(([k, v]) => {
+		if (pastInputStatus[k] !== v) {
+			inputChange(k, v)
+		}
+	})
+	pastInputStatus = inputStatus
+}
+
+setInterval(loop, 50)
+
+const start = () => {
+	window.addEventListener('deviceorientation', e => {
+		gamma = e.gamma
+	})
+	const id = document.querySelector('#id').value
+	const url = new URL(location.href)
+	ws = new WebSocket(`${url.protocol === 'http:' ? 'ws' : 'wss'}://${url.host}/api/ws/${id}`)
+}
+
+document.querySelector('.start').addEventListener('click', () => {
+	if (DeviceOrientationEvent.requestPermission) {
+		DeviceOrientationEvent.requestPermission()
+			.then((permissionState) => {
+				if (permissionState === "granted") {
+					start()
+				}
+			})
+			.catch(console.error)
+	}
+	start()
+})
