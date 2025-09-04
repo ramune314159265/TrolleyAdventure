cd67e608e0b19bc6132da0a78b544a684e5f22fc
ramune314159265 <ramune314159265@gmail.com>
2025-09-04 23:57:54 +0900

    サイズ調整

--- game/js/io/trolley/component/questionFirstInfo.js
+++ game/js/io/trolley/component/questionFirstInfo.js
@@ -10,8 +10,8 @@ export class QuestionFirstInfoComponent extends Container {
 	}
 	init() {
 		this.warningMark = Sprite.from('warning')
-		this.warningMark.width = 350
-		this.warningMark.height = 350
+		this.warningMark.width = 600
+		this.warningMark.height = 600
 		this.warningMark.anchor.set(0.5)
 		this.warningMark.y = 350
 		this.warningMark.filters = [
@@ -25,14 +25,14 @@ export class QuestionFirstInfoComponent extends Container {
 			content: '',
 			styleOverride: {
 				fill: '#d16668',
-				fontSize: 40,
+				fontSize: 72,
 				align: 'center',
 				letterSpacing: 16,
-				lineHeight: 60
+				lineHeight: 72
 			}
 		})
 		this.text.anchor.y = 0
-		this.text.y = 525
+		this.text.y = 650
 		this.addChild(this.text)
 		this.filters = [
 			new NoiseFilter({ noise: 0.3 })
@@ -40,18 +40,16 @@ export class QuestionFirstInfoComponent extends Container {
 	}
 	setInfo({ questionNo, level }) {
 		this.text.text = [
-			`! QUESTION ${questionNo} !`,
 			`LEVEL ${level}`
 		].join('\n')
 	}
 	async show() {
 		this.visible = true
 		for (let i = 0; i < 5; i++) {
-			this.alpha = (i % 2) === 0 ? 0.7 : 0
+			this.alpha = (i % 2) === 0 ? 0.8 : 0
 			await wait(100)
 		}
-		await wait(500)
-		this.alpha = 1
+		await wait(1000)
 	}
 	async hide() {
 		this.visible = false
