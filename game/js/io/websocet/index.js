24fbf02ba44987b1d95596e04aaa173fafe8e98d
ramune314159265 <ramune314159265@gmail.com>
2025-09-12 07:30:26 +0900

    再接続

--- game/js/io/websocet/index.js
+++ game/js/io/websocet/index.js
@@ -13,28 +13,36 @@ export class WebSocketIO extends GameIO {
 				this.ws.close()
 			}
 			const url = new URL(location.href)
-			this.ws = new WebSocket(`${url.protocol === 'http:' ? 'ws' : 'wss'}://${url.host}/api/ws/${configs.get('name')}`)
-			this.ws.onopen = () => {
-				session.onAny((eventname, data) => {
-					if (Object.keys(inputs).includes(eventname.description)) {
+			const connectWs = () => {
+				this.ws = new WebSocket(`${url.protocol === 'http:' ? 'ws' : 'wss'}://${url.host}/api/ws/${configs.get('name')}`)
+				this.ws.onopen = () => {
+					session.onAny((eventname, data) => {
+						if (Object.keys(inputs).includes(eventname.description)) {
+							return
+						}
+						this.ws.send(JSON.stringify({
+							event: eventname.description, data
+						}))
+					})
+				}
+				this.ws.onmessage = message => {
+					const data = JSON.parse(message.data)
+					const eventname = inputs[data.event]
+					if (!eventname) {
 						return
 					}
-					this.ws.send(JSON.stringify({
-						event: eventname.description, data
-					}))
-				})
-			}
-			this.ws.onmessage = message => {
-				const data = JSON.parse(message.data)
-				const eventname = inputs[data.event]
-				if (!eventname) {
-					return
+					session.emit(eventname)
 				}
-				session.emit(eventname)
+				this.ws.onclose = () => {
+					console.error('ws connect error')
+					setTimeout(() => connectWs(), 3000)
+				}
+				this.ws.onerror = () => {
+					console.error('ws connect error')
+					setTimeout(() => connectWs(), 3000)
+				}
 			}
-			this.ws.onerror = () => {
-				console.error('ws connect error')
-			}
+			connectWs()
 		})
 	}
 }
